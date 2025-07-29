7bc827dd15075ce0186aaef2d006107012385420
mahika1890 <mahika1890.be23@chitkara.edu.in>
2025-07-29 12:42:03 +0530

    done

---
+++ PersonalityQuiz1/PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by mahika behal on 12/13/18.
+//  Copyright © 2018 mahika behal. All rights reserved.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
